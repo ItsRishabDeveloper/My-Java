f5b5e5a8161cefef0ffcf752c14e66e994ec457e
Rishab Developer <itsrishabdeveloper@gmail.com>
2026-07-02 23:14:47 +0530

    Updated File

--- ArrayExercise2.java
+++ ArrayExercise2.java
@@ -1,15 +1,17 @@
 public class ArrayExercise2 {
     public static void main(String[] args) {
         int[] arr = {10,20,30,40,50,60,70,80,90,100};
-        for(int i=0;i<arr.length;i++){
+        for(int i=0;i<arr.length;i++) {
             System.out.println(arr[i]);
 
-      for(int j=0;j<arr.length;j++){
-          if(arr[i+1] > arr[j]){
-              String LargestNumber = String.valueOf(arr[i]);
-          }
-      }
-      System.out.println(LargestNumber);
+            String LargestNumber = null;
+            for (int k : arr) {
+                if (arr[i + 1] > k) {
+                    LargestNumber = String.valueOf(arr[i]);
+                    break;
+                }
+            }
+            System.out.println(LargestNumber);
         }
     }
 }
