c35ba1fd1f8b3d685df35f4af355d8cd4d1f995e
Rishab Developer <itsrishabdeveloper@gmail.com>
2026-07-11 23:56:33 +0530

    Part 2 done..

---
+++ Inheritance02.java
@@ -0,0 +1,17 @@
+class Person02{
+    String name;
+    int age;
+    Person02(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+}
+class Person022 extends Person02{
+    super()
+}
+public class Inheritance02 {
+    public static void main(String[] args) {
+        student s1=new student();
+
+    }
+}
