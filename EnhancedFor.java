29d32060d062fcb99c0746995a88e1e255bfe346
Rishab Developer <itsrishabdeveloper@gmail.com>
2026-08-10 14:12:20 +0530

    Add files via upload

---
+++ EnhancedFor.java
@@ -0,0 +1,8 @@
+public class EnhancedFor {
+    public static void main(String[] args) {
+        int arr[] = {10,20,30};
+        for(int nums : arr){
+            System.out.println(nums);
+        }
+    }
+}
